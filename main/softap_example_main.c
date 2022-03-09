b5a060e7c1865c8ad9c37d86aca1da6f8d559691
joel <joel_zerg_pro@outlook.com>
2022-03-08 23:55:49 -0500

    Task_TODO

--- main/softap_example_main.c
+++ main/softap_example_main.c
@@ -23,6 +23,10 @@
    If you'd rather not, just change the below entries to strings with
    the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
 */
+
+//TODO: make a web server
+
+
 #define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
 #define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 #define EXAMPLE_ESP_WIFI_CHANNEL   CONFIG_ESP_WIFI_CHANNEL
